0b4eed9c36c061142cca730f345cb01c19e793d3
Will <wmplqnjr@gmail.com>
2023-02-09 19:46:47 -0800

    Class 3

--- data.py
+++ data.py
@@ -10,3 +10,64 @@ me = {
         "zipcode":"78910"
     }
 }
+
+
+mock_catalog = [
+    {
+        "title":"Salmon",
+        "category": "frozen fish",
+        "image": "salmon.png",
+        "_id":"0000001",
+        "price": 5.01
+    },
+
+    {
+        "title":"Cod",
+        "category": "frozen fish",
+        "image": "cod.jpg",
+        "_id":"0000002",
+        "price": 3.35
+    },
+    {
+        "title":"Iceland Haddock",
+        "category": "fresh fish",
+        "image": "iceland haddock.jpg",
+        "_id":"0000003",
+        "price": 7.89
+    },
+    {
+        "title":"Mackrel",
+        "category": "fresh fish",
+        "image": "mackrel.jpg",
+        "_id":"0000004",
+        "price": 7.16
+    },
+    {
+        "title":"Seabass",
+        "category": "fresh fish",
+        "image": "seabass.jpg",
+        "_id":"0000005",
+        "price": 8.95
+    },
+    {
+        "title":"Tilapia",
+        "category": "frozen fish",
+        "image": "tilapia.jpg",
+        "_id":"0000006",
+        "price": 5.37
+    },
+    {
+        "title":"Tuna",
+        "category": "fresh fish",
+        "image": "tuna.jpg",
+        "_id":"0000007",
+        "price": 2.24
+    },
+    {
+        "title":"Pangasius",
+        "category": "fresh fish",
+        "image": "pangasius.jpg",
+        "_id":"0000008",
+        "price": 3.03
+    }
+]
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from data import me
+from flask import Flask, abort
+from data import me, mock_catalog
 
 import json
 
@@ -38,6 +38,68 @@ def address():
     address = me["address"]
     # return address["street"] + " #" + str(address["number"]) + ", " + address["city"] + ", " + address["zipcode"]
     return f'{address["street"]} #{address["number"]}, {address["city"]}, {address["zipcode"]}'
+
+
+
+
+
+@app.get("/api/catalog")
+def get_catalog():
+    return json.dumps(mock_catalog)
+
+
+
+@app.get("/api/catalog/count")
+def get_count():
+    count = len(mock_catalog)
+    return json.dumps(count)
+
+
+
+@app.get("/api/category/<cat>")
+def prods_by_category(cat):
+    results = []
+    for prod in mock_catalog:
+        if prod["category"] == cat:
+            results.append(prod)
+
+
+    return json.dumps(results)        
+
+
+
+@app.get("/api/product/<id>")
+def prods_by_id(id):
+    for prod in mock_catalog:
+        if prod["_id"] == id:
+            return json.dumps(prod) 
+        
+        
+    return abort(404, "Invalid id")
+
+
+
+@app.get("/api/product/search/<text>")
+def search_prods(text):
+    results = []
+    for prod in mock_catalog:
+        if text.lower() in prod["title"].lower():
+            results.append(prod)
+
+
+    return json.dumps(results)    
+
+
+@app.get("/api/categories")
+def get_categories():
+    results = []
+    for prod in mock_catalog:
+        cat = prod["category"]
+        if cat != prod["category"]:
+            results.append(prod["category"])
+
+
+    return json.dumps(results)   
 
 
 app.run(debug=True)
